cbb3a528555ba21d3a96458123f1924d085abcf3
Mac <mac@Macs-MacBook-Pro.local>
2022-04-08 18:43:19 +0200

    nodejs

---
+++ src/main.js
@@ -0,0 +1,12 @@
+function Main() {
+  var http = require("http");
+
+  http
+    .createServer(function (req, res) {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Hello World!");
+    })
+    .listen(8080);
+}
+
+export default Main;
